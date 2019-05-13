723f4a7eecee234dbf4d392bd0c8764ee7d4a14b
aldumas <adumas+dev@gmail.com>
2019-05-13 12:16:47 -0400

    Changed quotes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
-var util = require('util');
-var fsm = require('../src/fsm.js');
+var assert = require("assert");
+var util = require("util");
+var fsm = require("../src/fsm.js");
 
-describe('when transitioning from state to state', function() {
-    it('should invoke entry, exit, and action callbacks', function(done) {
+describe("when transitioning from state to state", function() {
+    it("should invoke entry, exit, and action callbacks", function(done) {
         let callbackCount = 0;
         const EXPECTED_CALLBACK_COUNT = 3;
 
@@ -14,7 +14,7 @@ describe('when transitioning from state to state', function() {
                     exit: () => ++callbackCount,
                     transitions: {
                         EVENT: {
-                            nextState: 'END',
+                            nextState: "END",
                             action: () => {
                                 ++callbackCount;
                                 done(callbackCount !== EXPECTED_CALLBACK_COUNT);
@@ -26,7 +26,7 @@ describe('when transitioning from state to state', function() {
         });
 
         machine.postStart();
-        machine.postEvent('EVENT');
+        machine.postEvent("EVENT");
     });
 
     it("should invoke current state's exit(), then the event's action(), then the next state's entry()", function(done) {
@@ -36,17 +36,17 @@ describe('when transitioning from state to state', function() {
         const machine = fsm.createMachine({
             spec: {
                 START: {
-                    exit: () => callbacksCalled.push('exit'),
+                    exit: () => callbacksCalled.push("exit"),
                     transitions: {
                         EVENT: {
-                            nextState: 'END',
-                            action: () =>  callbacksCalled.push('action')
+                            nextState: "END",
+                            action: () =>  callbacksCalled.push("action")
                         }
                     }
                 },
                 END: {
                     entry: () => {
-                        callbacksCalled.push('entry');
+                        callbacksCalled.push("entry");
                         done(!util.isDeepStrictEqual(callbacksCalled, EXPECTED_CALLBACKS_CALLED));
                     }
                 },
@@ -54,31 +54,31 @@ describe('when transitioning from state to state', function() {
         });
 
         machine.postStart();
-        machine.postEvent('EVENT');
+        machine.postEvent("EVENT");
     });
 
-    describe('when ignoreUnexpectedEvents == false', function() {
-        it('should error if event is received before machine is started', function(done) {
+    describe("when ignoreUnexpectedEvents == false", function() {
+        it("should error if event is received before machine is started", function(done) {
             const machine = fsm.createMachine({
                 spec: {
                     START: {
                         transitions: {
                             EVENT: {
-                                nextState: 'END'
+                                nextState: "END"
                             }
                         }
                     }
                 }
             });
 
-            assert.rejects(machine.postEvent('EVENT'))
+            assert.rejects(machine.postEvent("EVENT"))
                 .then(() => done())
                 .catch(() => done(true));
         });
     });
 
-    describe('when ignoreUnexpectedEvents == true', function() {
-        it('should not error if event is received before machine is started', function(done) {
+    describe("when ignoreUnexpectedEvents == true", function() {
+        it("should not error if event is received before machine is started", function(done) {
             const machine = fsm.createMachine({
                 options: {
                     ignoreUnexpectedEvents: true
@@ -87,14 +87,14 @@ describe('when transitioning from state to state', function() {
                     START: {
                         transitions: {
                             EVENT: {
-                                nextState: 'END'
+                                nextState: "END"
                             }
                         }
                     }
                 }
             });
 
-            assert.doesNotReject(machine.postEvent('EVENT'))
+            assert.doesNotReject(machine.postEvent("EVENT"))
                 .then(() => done())
                 .catch(() => done(true));
         });
