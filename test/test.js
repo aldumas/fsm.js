ec51a8770e22c0ffea639f3f8f7d2cbe9c4bf177
aldumas <adumas+dev@gmail.com>
2019-05-13 12:14:30 -0400

    Fixed tests. Added some more.

--- test/test.js
+++ test/test.js
@@ -1,31 +1,23 @@
 var assert = require('assert');
+var util = require('util');
 var fsm = require('../src/fsm.js');
 
-/*
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-*/
+describe('when transitioning from state to state', function() {
+    it('should invoke entry, exit, and action callbacks', function(done) {
+        let callbackCount = 0;
+        const EXPECTED_CALLBACK_COUNT = 3;
 
-describe('fsm.js', function() {
-    it('should invoke all user-defined callbacks', function(done) {
-        let callbacksCalled = 0;
-
-        let machine = fsm.createMachine({
+        const machine = fsm.createMachine({
             spec: {
                 START: {
-                    entry: () => ++callbacksCalled,
-                    exit: () => ++callbacksCalled,
+                    entry: () => ++callbackCount,
+                    exit: () => ++callbackCount,
                     transitions: {
                         EVENT: {
                             nextState: 'END',
                             action: () => {
-                                ++callbacksCalled;
-                                done(callbacksCalled !== 3);
+                                ++callbackCount;
+                                done(callbackCount !== EXPECTED_CALLBACK_COUNT);
                             }
                         }
                     }
@@ -35,5 +27,76 @@ describe('fsm.js', function() {
 
         machine.postStart();
         machine.postEvent('EVENT');
+    });
+
+    it("should invoke current state's exit(), then the event's action(), then the next state's entry()", function(done) {
+        const callbacksCalled = [];
+        const EXPECTED_CALLBACKS_CALLED = ["exit", "action", "entry"];
+
+        const machine = fsm.createMachine({
+            spec: {
+                START: {
+                    exit: () => callbacksCalled.push('exit'),
+                    transitions: {
+                        EVENT: {
+                            nextState: 'END',
+                            action: () =>  callbacksCalled.push('action')
+                        }
+                    }
+                },
+                END: {
+                    entry: () => {
+                        callbacksCalled.push('entry');
+                        done(!util.isDeepStrictEqual(callbacksCalled, EXPECTED_CALLBACKS_CALLED));
+                    }
+                },
+            }
+        });
+
+        machine.postStart();
+        machine.postEvent('EVENT');
+    });
+
+    describe('when ignoreUnexpectedEvents == false', function() {
+        it('should error if event is received before machine is started', function(done) {
+            const machine = fsm.createMachine({
+                spec: {
+                    START: {
+                        transitions: {
+                            EVENT: {
+                                nextState: 'END'
+                            }
+                        }
+                    }
+                }
+            });
+
+            assert.rejects(machine.postEvent('EVENT'))
+                .then(() => done())
+                .catch(() => done(true));
+        });
+    });
+
+    describe('when ignoreUnexpectedEvents == true', function() {
+        it('should not error if event is received before machine is started', function(done) {
+            const machine = fsm.createMachine({
+                options: {
+                    ignoreUnexpectedEvents: true
+                },
+                spec: {
+                    START: {
+                        transitions: {
+                            EVENT: {
+                                nextState: 'END'
+                            }
+                        }
+                    }
+                }
+            });
+
+            assert.doesNotReject(machine.postEvent('EVENT'))
+                .then(() => done())
+                .catch(() => done(true));
+        });
     });
 });
